3ebf123a5f5432dc6af97b3f224751a8134a1986
diegoami <diego.amicabile@gmail.com>
2017-08-08 23:38:09 +0200

    Removed stuff in out directory

--- out/artifacts/Chessella_Restored_war_exploded/WEB-INF/classes/tools/compileclass.sh
+++
@@ -1,1 +0,0 @@
-javac -target 1.5 -source 1.5 -cp source:lib/* source/com/amicabile/openingtrainer/dao/GameDataObjDAO.java 
--- out/artifacts/Chessella_Restored_war_exploded/dwr-js/UrlValidator.js
+++
@@ -1,6 +0,0 @@
-function UrlValidator() { }
-UrlValidator._path = 'dwr';
-
-UrlValidator.isValid = function(p0, callback) {
-    DWREngine._execute(UrlValidator._path, 'UrlValidator', 'isValid', p0, callback);
-}
